3757f565bd8cb6a2c5db708e57f740e99655daa6
bals <reancirl@gmail.com>
2023-02-23 15:18:17 +0800

    [initial] controller,model and migration file created

---
+++ app/Http/Controllers/PriceBookBundleController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PriceBookBundle;
+use Illuminate\Http\Request;
+
+class PriceBookBundleController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PriceBookBundle  $priceBookBundle
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PriceBookBundle $priceBookBundle)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PriceBookBundle  $priceBookBundle
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PriceBookBundle $priceBookBundle)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PriceBookBundle  $priceBookBundle
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PriceBookBundle $priceBookBundle)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PriceBookBundle  $priceBookBundle
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PriceBookBundle $priceBookBundle)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/PriceBookEntryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PriceBookEntry;
+use Illuminate\Http\Request;
+
+class PriceBookEntryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PriceBookEntry  $priceBookEntry
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PriceBookEntry $priceBookEntry)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PriceBookEntry  $priceBookEntry
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PriceBookEntry $priceBookEntry)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PriceBookEntry  $priceBookEntry
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PriceBookEntry $priceBookEntry)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PriceBookEntry  $priceBookEntry
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PriceBookEntry $priceBookEntry)
+    {
+        //
+    }
+}
